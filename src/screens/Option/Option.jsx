83c96de17ceaccdc523657e3cb4ace549d4c5d41
Könül_Heart <100348091+KonulMemmedova@users.noreply.github.com>
2023-10-03 00:50:48 +0400

    Initial commit

---
+++ src/screens/Option/Option.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export const Option = () => {
+  return (
+    <div className="option">
+      <div className="overlap">
+        <div className="text-wrapper">Sign Up</div>
+      </div>
+      <div className="group">
+        <div className="overlap-group">
+          <div className="div">Sign In</div>
+        </div>
+      </div>
+      <div className="text-wrapper-2">Forgot Password?</div>
+    </div>
+  );
+};
